eb25815342bd0bbba1148b47532bbe8054ce47c0
meadlai <laiqinyi@gmail.com>
2014-10-29 10:32:16 +0800

    add servlet

---
+++ source/main/com/meadidea/java/server/Start.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.meadidea.java.server;
+
+/**
+ * @author meadlai
+ *
+ */
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+	}
+
+}
