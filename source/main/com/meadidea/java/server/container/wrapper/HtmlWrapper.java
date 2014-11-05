869d3a63f2874692c904064e4194f648478f93e3
meadlai <laiqinyi@gmail.com>
2014-11-05 09:03:05 +0800

    request & response

---
+++ source/main/com/meadidea/java/server/container/wrapper/HtmlWrapper.java
@@ -0,0 +1,5 @@
+package com.meadidea.java.server.container.wrapper;
+
+public class HtmlWrapper {
+
+}
--- source/main/com/meadidea/java/server/http/HttpResponse.java
+++ source/main/com/meadidea/java/server/http/HttpResponse.java
@@ -1,15 +1,269 @@
 package com.meadidea.java.server.http;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
-public class HttpResponse {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class HttpResponse implements HttpServletResponse{
 	//
 	public HttpResponse(OutputStream os){
-		
+		this.output = os;
 	}
 
 	public void setRequest(HttpRequest request) {
+		this.request = request;
+	}
+
+	
+	
+	  // the default buffer size
+	  private static final int BUFFER_SIZE = 1024;
+	  HttpRequest request;
+	  OutputStream output;
+	  PrintWriter writer;
+	  protected byte[] buffer = new byte[BUFFER_SIZE];
+	  protected int bufferCount = 0;
+	  /**
+	   * Has this response been committed yet?
+	   */
+	  protected boolean committed = false;
+	  /**
+	   * The actual number of bytes written to this Response.
+	   */
+	  protected int contentCount = 0;
+	  /**
+	   * The content length associated with this Response.
+	   */
+	  protected int contentLength = -1;
+	  /**
+	   * The content type associated with this Response.
+	   */
+	  protected String contentType = null;
+	  /**
+	   * The character encoding associated with this Response.
+	   */
+	  protected String encoding = null;
+
+	  /**
+	   * The set of Cookies associated with this Response.
+	   */
+	  protected ArrayList cookies = new ArrayList();
+	  /**
+	   * The HTTP headers explicitly added via addHeader(), but not including
+	   * those to be added with setContentLength(), setContentType(), and so on.
+	   * This collection is keyed by the header name, and the elements are
+	   * ArrayLists containing the associated values that have been set.
+	   */
+	  protected HashMap headers = new HashMap();
+	  
+	  protected int status = HttpServletResponse.SC_OK;
+
+	@Override
+	public String getCharacterEncoding() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getContentType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ServletOutputStream getOutputStream() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrintWriter getWriter() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCharacterEncoding(String charset) {
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void setContentLength(int len) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setContentType(String type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBufferSize(int size) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getBufferSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void flushBuffer() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resetBuffer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isCommitted() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLocale(Locale loc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Locale getLocale() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addCookie(Cookie cookie) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsHeader(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String encodeURL(String url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String encodeRedirectURL(String url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String encodeUrl(String url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String encodeRedirectUrl(String url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendError(int sc, String msg) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendError(int sc) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendRedirect(String location) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDateHeader(String name, long date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addDateHeader(String name, long date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHeader(String name, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addHeader(String name, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIntHeader(String name, int value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addIntHeader(String name, int value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStatus(int sc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStatus(int sc, String sm) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
